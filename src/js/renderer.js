80a14427ae8ce57218c4aebe4398abbbbb5945fa
Gabriel Oliver <gabrielmed2010@hotmail.com>
2021-04-08 18:21:03 -0300

    fix: ajusta função de exportar do abrirSite

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,4 +1,4 @@
-const abrirSite = require("./abrirSite");
+const { abrirSite } = require("./abrirSite");
 
 let botaoSite = document.querySelector(".ir-site-botao");
 botaoSite.addEventListener("click", abrirSite);
